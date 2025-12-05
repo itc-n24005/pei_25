48eccab847119715f0ece808f094a183ccc4a308
itc-n24005 <n24005@std.it-college.ac.jp>
2025-12-05 01:02:58 +0000

    m3_q3kadai.py

---
+++ m3/m3_q3kadai.py
@@ -0,0 +1,15 @@
+# ① list_a に [-4, 5, 6, 'Hello', 'World', 7, -8, 9] を格納してください
+list_a = [-4, 5, 6, 'Hello', 'World', 7, -8, 9]
+
+# ② list_a[6] の絶対値を出力してください
+print(abs(list_a[6]))
+
+# ③ list_a[0] + list_a[1] の bool 値を出力してください
+print(bool(list_a[0] + list_a[1]))
+
+# ④ list_a[2] の 2 進数表示（0bを外して表示）
+print(bin(list_a[2])[2:])
+
+# ⑤ list_a[3] と list_a[4] の文字列を連結して表示
+print(list_a[3] + list_a[4])
+
